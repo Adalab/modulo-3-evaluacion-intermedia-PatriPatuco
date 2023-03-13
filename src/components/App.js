2cf60170fe4c16bf2e4a6209fde976a01f99ce53
Patuco <patricia.beco.dg@gmail.com>
2023-03-13 09:22:41 +0100

    Add basic HTML

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,49 @@
-/* SECCIÓN DE IMPORT */
-
-// - De React
-// - Nuestros
-// - Sass
+import { useState } from "react";
+import api from "../data/api.json";
 import '../styles/App.scss';
-// - Imágenes
 
-/* SECCIÓN DEL COMPONENTE */
 function App() {
   /* VARIABLES ESTADO (DATOS) */
-
-  /* EFECTOS (día 5) */
-
+const [data, setData] = useState(api);
+/* const [quotes, setQuotes] = useState(""); */
   /* FUNCIONES HANDLER */
 
   /* FUNCIONES Y VARIABLES AUXILIARES PARA PINTAR EL HTML */
+  const renderQuotes = () => {
+    return data.map((eachQuote, index) => (
+      <li className="quote-item" key={index}>
+        <p className="quote-p">
+          {eachQuote.quote} - <span className="char-name">{eachQuote.character}</span>
+        </p>
+      </li>
+    ));
+  }
 
   /* HTML */
-  return <div className="App">{/* Aquí va el HTML */}</div>;
+  return (
+    <div className="App">
+      <section className="header">
+        <h1 className="main-title">Frases de Friends</h1>
+        <form action="">
+          <label htmlFor="search-quote" className="search">Filtrar por frase</label>
+          <input type="text" className="search-quote in-search" />
+          <label htmlFor="search-char" className="search">Filtrar por personaje</label>
+          <select className="search-char in-search">
+            <option value="Ross">Ross</option>
+            <option value="Joey">Joey</option>
+            <option value="Phoebe">Phoebe</option>
+            <option value="Chandler">Chandler</option>
+            <option value="Rachel">Rachel</option>
+          </select>
+        </form>
+      </section>
+      <main>
+        <ul className="quotes-list">
+          {renderQuotes()}
+        </ul>
+      </main>
+    </div>
+  );
 }
 
-/* PROP-TYPES */
-
-/* EXPORT DEL COMPONENTE */
 export default App;
