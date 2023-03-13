0f639cdbf820ecb22a588b792fdbc8d31ea643f0
Patuco <patricia.beco.dg@gmail.com>
2023-03-13 09:39:01 +0100

    Add quote filter function

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,33 @@
 import { useState } from "react";
 import api from "../data/api.json";
-import '../styles/App.scss';
+import "../styles/App.scss";
 
 function App() {
-  /* VARIABLES ESTADO (DATOS) */
-const [data, setData] = useState(api);
-/* const [quotes, setQuotes] = useState(""); */
-  /* FUNCIONES HANDLER */
+  const [data, setData] = useState(api);
+  const [quotes, setQuotes] = useState("");
+  const [character, setCharacter] = useState ("Todos");
 
-  /* FUNCIONES Y VARIABLES AUXILIARES PARA PINTAR EL HTML */
+  /* --PRINT LIST-- */
   const renderQuotes = () => {
-    return data.map((eachQuote, index) => (
+
+    /* Filter to show the quotes matching the user quote search */
+    return data
+    .filter ((eachQuote) => {
+      return eachQuote.quote.toLowerCase().includes(quotes.toLowerCase());
+    })
+    .map((eachQuote, index) => (
       <li className="quote-item" key={index}>
         <p className="quote-p">
-          {eachQuote.quote} - <span className="char-name">{eachQuote.character}</span>
+          {eachQuote.quote} -{" "}
+          <span className="char-name">{eachQuote.character}</span>
         </p>
       </li>
     ));
-  }
+  };
+
+  const handleQuote = (ev) => {
+    setQuotes(ev.target.value);
+  };
 
   /* HTML */
   return (
@@ -25,11 +35,21 @@ const [data, setData] = useState(api);
       <section className="header">
         <h1 className="main-title">Frases de Friends</h1>
         <form action="">
-          <label htmlFor="search-quote" className="search">Filtrar por frase</label>
-          <input type="text" className="search-quote in-search" />
-          <label htmlFor="search-char" className="search">Filtrar por personaje</label>
+          <label htmlFor="search-quote" className="search">
+            Filtrar por frase
+          </label>
+          <input 
+          type="text" 
+          className="search-quote in-search"
+          value={quotes}
+          onChange={handleQuote} />
+          <label htmlFor="search-char" className="search">
+            Filtrar por personaje
+          </label>
           <select className="search-char in-search">
+            <option value="Todos">Todos</option>
             <option value="Ross">Ross</option>
+            <option value="Monica">Monica</option>
             <option value="Joey">Joey</option>
             <option value="Phoebe">Phoebe</option>
             <option value="Chandler">Chandler</option>
@@ -38,9 +58,7 @@ const [data, setData] = useState(api);
         </form>
       </section>
       <main>
-        <ul className="quotes-list">
-          {renderQuotes()}
-        </ul>
+        <ul className="quotes-list">{renderQuotes()}</ul>
       </main>
     </div>
   );
